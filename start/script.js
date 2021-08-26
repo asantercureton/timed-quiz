1d3b27952f8184fedb2e4976012ca6a7c7956c7c
Asante Cureton <asantercureton@gmail.com>
2021-08-25 22:33:22 -0400

    Updated Javascript file

--- start/script.js
+++ start/script.js
@@ -1,62 +1,113 @@
-// Identify variables
-var startBtn = document.querySelector('#startBtn');
-var question = document.querySelector('#question');
-var answer = document.querySelector('#answer');
-var timer = document.querySelector('#timer');
-var timeLeft = 60;
-var questions = ["What is JavaScript?, Who founded JavaScript?, Why is JavaScript important?, When was JavaScript first introduced?"];
-var answers = ["A1, A2, A3, A4"];
+// Identify variables from html
+var timer = document.getElementById('#timer');
+var question = document.getElementById('#question');
+// Brings in choices as an array
+var choices = Array.from(document.getElementById('#.choice-text'));
+var time = 60;
+// var questions = ["What is JavaScript?, Who founded JavaScript?, Why is JavaScript important?, When was JavaScript first introduced?"];
+// var answers = ["A1, A2, A3, A4"];
 
-function renderQuestion() {
-    var selected = Math.floor(Math.random() * questions.length);
-    questionEl.children[0];
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var questionCounter = 0;
+var availableQuestions = [];
+
+// Start Timer
+function startTimer() {
+    var timer = setInterval(() => {
+        timeLeft--
+        displayTime.textContent = timeLeft + " seconds remaining..."
+    }, 1000);
 }
 
-
-// // Creating the questions using objects and arrays
-// var myQuestions = [
-//     // question 1 with answer choices and correct answer
-//     {
-//         question: "Who?",
-//         answer: ["A", "B", "C", "D"],
-//         correct: 1
-//     },
-//     // question 2 with answer choices and correct answer
-//     {
-//         question: "Who?",
-//         answer: ["A", "B", "C", "D"],
-//         correct: 2
-//     },
-//     // question 3 with answer choices and correct answer
-//     {
-//         question: "Who?",
-//         answer: ["A", "B", "C", "D"],
-//         correct: 0
-//     },
-//     // question 4 with answer choices and correct answer
-//     {
-//         question: "Who?",
-//         answer: ["A", "B", "C", "D"],
-//         correct: 2
-//     },
-//     // question 5 with answer choices and correct answer
-//     {
-//         question: "Who?",
-//         answer: ["A", "B", "C", "D"],
-//         correct: 3
-//     },
-// ];
+// Creating the questions using objects and arrays
+var myQuestions = [
+    // question 1 with answer choices and correct answer
+    {
+        question: "Who?",
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: 1
+    },
+    // question 2 with answer choices and correct answer
+    {
+        question: "Who?",
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: 1
+    },
+    // question 3 with answer choices and correct answer
+    {
+        question: "Who?",
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: 1
+    },
+    // question 4 with answer choices and correct answer
+    {
+        question: "Who?",
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: 1
+    },
+    // question 5 with answer choices and correct answer
+    {
+        question: "Who?",
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: 1
+    },
+];
 
 
 // Once start button is clicked, start timer and present first question
-startBtn.addEventListener('click', startQuiz) {
-    // Start Timer
-    function startTimer() {
-        var timer = setInterval(() => {
-        timeLeft--
-        displayTime.textContent = timeLeft + " seconds remaining..."
-        }, 1000);
+startQuiz = () => {
+    questionCounter = 0
+    time = 60
+    availableQuestions = [...myQuestions]
+    nextQuestion();
+}
+
+nextQuestion = () => {
+    if(availableQuestions === 0) {
+        localStorage.setItem('mostRecentScore', time)
+        return window.location.assign('/end.html')
     }
+
+    questionCounter++
+
+    var questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        var number = choice.dataset('number');
+        choice.innerText = currentQuestion['choice' + number];
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    if(acceptingAnswers) return
+
+    acceptingAnswers = false;
+
+})
+
+
 
     getQuestions() {
         for (var i = 0; i > 0; i--) {
